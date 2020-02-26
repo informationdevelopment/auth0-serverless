272a52b247a934d51120ad3d98664f47030f1c02
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-26 10:20:47 -0800

    Clean up try/catch in index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ const authorize = require('./authorize');
 
 module.exports.createClient = (options) => ({
     authorize: (scope, handler) => async (...args) => {
-        try {
         const wrapper = serverless.getWrapper(args);
         const result = await authorize({ ...options, scope, wrapper });
         if (result.authorized) {
             return handler(...args);
         }
         return result.errorResponse;
-        }
-        catch (err) {
-            return {
-                statusCode: 500,
-                body: err.message + 'FFFFF',
-            };
-        }
     },
 
     authorizeSync: (scope, handler) => (...args) => {
